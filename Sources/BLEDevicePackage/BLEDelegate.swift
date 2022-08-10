ad2c516856dcaf89b19e4ae2130fd751ed7dcad0
miokato <miokato@me.com>
2022-08-10 09:29:19 +0900

    Update.

--- Sources/BLEDevicePackage/BLEDelegate.swift
+++ Sources/BLEDevicePackage/BLEDelegate.swift
@@ -5,18 +5,21 @@
 //  Created by mio kato on 2022/04/21.
 //
 
-import Foundation
 import CoreBluetooth
+import Foundation
 
-
-@objc public protocol BLEDelegate {
+public protocol BLEDelegate: AnyObject {
     func deviceFound(devName: String, mfgID: String, deviceID: String)
     func didConnect()
     func didDisconnect()
     func eegSampleLeft(_ left: Int32, right: Int32)
     func sensorStatus(_ status: Int32)
     func battery(_ percent: Int32)
-    @objc optional func centralManagerDidUpdateState(_ central: CBCentralManager)
-    @objc optional func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?)
+    func centralManagerDidUpdateState(_ central: CBCentralManager)
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?)
 }
 
+public extension BLEDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {}
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {}
+}
--- Sources/BLEDevicePackage/BLEDevice.swift
+++ Sources/BLEDevicePackage/BLEDevice.swift
@@ -5,13 +5,12 @@
 //  Created by mio kato on 2022/04/11.
 //
 
-import Foundation
 import CoreBluetooth
 
 #if os(iOS)
 
 /// BLEDevice UUID
-fileprivate enum DeviceUUID: String {
+private enum DeviceUUID: String {
     // Device infomation (Read)
     case deviceInfoService = "180A"
     case manufacturerCharacteristic = "2A29"
@@ -30,160 +29,152 @@ fileprivate enum DeviceUUID: String {
     // Notify
     case statusCharacteristic = "0B79FFB0-1ED1-2840-A9C3-87C6F6186DB3"
     case streamCharacteristic = "0B79FFF6-1ED1-2840-A9C3-87C6F6186DB3"
-    
+
     var uuid: CBUUID {
-        return CBUUID(string: self.rawValue)
+        CBUUID(string: rawValue)
     }
 }
 
-
-public class BLEDevice: NSObject {
-    
+public final class BLEDevice: NSObject {
     public static var shared = BLEDevice()
-    
-    private var delegate: BLEDelegate?
-    
+
+    private weak var delegate: BLEDelegate?
+
     private var centralManager: CBCentralManager!
-    
+
     // 接続されたペリフェラル
     private var connectedPeripheral: CBPeripheral?
-    
+
     private var modeCharacteristic: CBCharacteristic?
     private var batteryCharacteristic: CBCharacteristic?
-    
+
     // 脳波検出開始コード
     private let startBytes: [UInt8] = [
         0x77, 0x01, 0x01, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0xfd
+        0x00, 0x00, 0x00, 0x00, 0xFD
     ]
     // 脳波検出停止コード
     private let stopBytes: [UInt8] = [
         0x77, 0x01, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0xfe
+        0x00, 0x00, 0x00, 0x00, 0xFE
     ]
-    
+
     let queue: DispatchQueue
-    
-    private override init() {
+
+    override private init() {
         queue = DispatchQueue(label: "BLEDevice.bleQueue")
-        
+
         super.init()
-        
+
         centralManager = CBCentralManager(delegate: self, queue: queue)
     }
-    
+
     public func setDelegate(delegate: BLEDelegate) {
         self.delegate = delegate
     }
-    
+
     /// スキャン開始
     public func scanDevice() {
-        let services = [
-            DeviceUUID.deviceInfoService.uuid,
-            DeviceUUID.batteryService.uuid,
-            DeviceUUID.eegService.uuid
-        ]
+        let services = [DeviceUUID.deviceInfoService.uuid]
         centralManager.scanForPeripherals(withServices: services, options: nil)
     }
-    
+
     /// スキャン停止
     public func stopScanDevice() {
         centralManager.stopScan()
     }
-    
+
     /// 接続
     public func connect(_ deviceID: String) {
         guard let lastUUID = UUID(uuidString: deviceID),
-              let peripheral = centralManager.retrievePeripherals(withIdentifiers: [lastUUID]).first  else {
+              let peripheral = centralManager.retrievePeripherals(withIdentifiers: [lastUUID]).first else {
             return
         }
         // 強参照が必要なのでプロパティとして保持
         connectedPeripheral = peripheral
         centralManager.connect(peripheral,
-                               options: [CBConnectPeripheralOptionEnableTransportBridgingKey : true])
+                               options: [CBConnectPeripheralOptionEnableTransportBridgingKey: true])
     }
-    
+
     /// 接続を解除
     public func disconnectDevice() {
         guard let peripheral = connectedPeripheral else {
             return
         }
 
-        for service in (peripheral.services ?? [] as [CBService]) {
-            for characteristic in (service.characteristics ?? [] as [CBCharacteristic]) {
-                if characteristic.uuid == DeviceUUID.statusCharacteristic.uuid && characteristic.isNotifying {
+        for service in peripheral.services ?? [] as [CBService] {
+            for characteristic in service.characteristics ?? [] as [CBCharacteristic] {
+                if characteristic.uuid == DeviceUUID.statusCharacteristic.uuid, characteristic.isNotifying {
                     peripheral.setNotifyValue(false, for: characteristic)
                 }
-                if characteristic.uuid == DeviceUUID.modeCharacteristic.uuid && characteristic.isNotifying {
+                if characteristic.uuid == DeviceUUID.modeCharacteristic.uuid, characteristic.isNotifying {
                     peripheral.setNotifyValue(false, for: characteristic)
                 }
             }
         }
         centralManager.cancelPeripheralConnection(peripheral)
     }
-    
+
     /// 脳波の検出を開始
     public func start() {
         guard let peripheral = connectedPeripheral,
               let modeCharacteristic = modeCharacteristic else {
             return
         }
-        
+
         peripheral.writeValue(Data(startBytes), for: modeCharacteristic, type: .withResponse)
     }
-    
+
     /// 脳波の検出を停止
     public func stop() {
         guard let peripheral = connectedPeripheral,
               let modeCharacteristic = modeCharacteristic else {
             return
         }
-        
+
         peripheral.writeValue(Data(stopBytes), for: modeCharacteristic, type: .withResponse)
     }
 }
 
 extension BLEDevice: CBCentralManagerDelegate {
     /// ペリフェラルを発見した
-    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
         guard let deviceName = peripheral.name else {
             return
         }
-        
+
         delegate?.deviceFound(devName: deviceName, mfgID: peripheral.identifier.uuidString, deviceID: peripheral.identifier.uuidString)
     }
-    
+
     /// ペリフェラルに接続された
     public func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         peripheral.delegate = self
         peripheral.discoverServices([
             DeviceUUID.eegService.uuid,
-            DeviceUUID.batteryService.uuid,
+            DeviceUUID.batteryService.uuid
         ])
         connectedPeripheral = peripheral
-        
-        delegate?.didConnect()
     }
-    
+
     /// ペリフェラルと接続が解除された
     public func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
         delegate?.didDisconnect()
-        
+
         connectedPeripheral = nil
     }
-    
+
     /// ペリフェラルとの接続に失敗した
     public func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
-        delegate?.centralManager?(central, didFailToConnect: peripheral, error: error)
+        delegate?.centralManager(central, didFailToConnect: peripheral, error: error)
     }
-    
+
     /// 状態が変化した
     public func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        delegate?.centralManagerDidUpdateState?(central)
+        delegate?.centralManagerDidUpdateState(central)
         switch central.state {
         case .poweredOn:
             break
@@ -196,6 +187,7 @@ extension BLEDevice: CBCentralManagerDelegate {
 }
 
 // MARK: - Peripheral Delegate
+
 extension BLEDevice: CBPeripheralDelegate {
     /// サービスを発見した
     public func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
@@ -203,25 +195,25 @@ extension BLEDevice: CBPeripheralDelegate {
             print("Error discovering services: %s", error.localizedDescription)
             return
         }
-        
+
         guard let peripheralServices = peripheral.services else { return }
         for service in peripheralServices {
             peripheral.discoverCharacteristics([
                 DeviceUUID.modeCharacteristic.uuid,
                 DeviceUUID.statusCharacteristic.uuid,
                 DeviceUUID.streamCharacteristic.uuid,
-                DeviceUUID.batteryCharacteristic.uuid,
+                DeviceUUID.batteryCharacteristic.uuid
             ], for: service)
         }
     }
-    
+
     /// キャラクタリスティックを発見した
     public func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         if let error = error {
             print("Error discovering characteristics: %s", error.localizedDescription)
             return
         }
-        
+
         guard let serviceCharacteristics = service.characteristics else { return }
         for characteristic in serviceCharacteristics {
             if characteristic.uuid == DeviceUUID.statusCharacteristic.uuid {
@@ -238,27 +230,28 @@ extension BLEDevice: CBPeripheralDelegate {
                 batteryCharacteristic = characteristic
             }
         }
+        delegate?.didConnect()
     }
-    
+
     /// connectした時の呼ばれる
     public func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
         if let error = error {
             print(error.localizedDescription)
         }
     }
-    
+
     /// Notify属性の値更新時に呼ばれる(ここの脳波データをハンドリング)
     public func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         if let error = error {
             print(error.localizedDescription)
         }
-        
+
         if characteristic.uuid == DeviceUUID.statusCharacteristic.uuid {
             // ステータス更新
             if let data = characteristic.value {
                 handleEEGStatus(data: data)
             }
-            
+
         } else if characteristic.uuid == DeviceUUID.streamCharacteristic.uuid {
             if let data = characteristic.value {
                 handleEEGSignal(data: data)
@@ -267,28 +260,28 @@ extension BLEDevice: CBPeripheralDelegate {
             handleBatteryStatus(characteristic: characteristic)
         }
     }
-    
+
     /// 脳波データを送信
     private func handleEEGSignal(data: Data) {
         let index: UInt8 = data[0]
         let status: UInt8 = data[1]
-        let leftData = data[2...41]
-        let rightData = data[42...data.count-1]
+        let leftData = data[2 ... 41]
+        let rightData = data[42 ... data.count - 1]
         let leftValues = leftData.encodedInt16
         let rightValues = rightData.encodedInt16
-        
+
         // 1秒ごとにステータスを送信
         if index == 0 {
             handleSensorStatus(status: Int(status))
             readBattery()
         }
-        
+
         // 600fpsで脳波を送信 (30fps x 20data)
         for (leftValue, rightValue) in zip(leftValues, rightValues) {
-            self.delegate?.eegSampleLeft(Int32(leftValue), right: Int32(rightValue))
+            delegate?.eegSampleLeft(Int32(leftValue), right: Int32(rightValue))
         }
     }
-    
+
     /// バッテリー容量の読み出し
     private func readBattery() {
         guard let peripheral = connectedPeripheral,
@@ -297,7 +290,7 @@ extension BLEDevice: CBPeripheralDelegate {
         }
         peripheral.readValue(for: batteryCharacteristic)
     }
-    
+
     /// バッテリー容量の読み出しのコールバック
     private func handleBatteryStatus(characteristic: CBCharacteristic) {
         guard let data = characteristic.value else {
@@ -306,29 +299,30 @@ extension BLEDevice: CBPeripheralDelegate {
         let batteryPercent = data.encodedUInt8[0]
         delegate?.battery(Int32(batteryPercent))
     }
-    
+
     /// 脳波デバイスの装着ステータスを更新 [0 : ok, 1 : left-x, 2 : right-x, 3 : both-x]
     private func handleSensorStatus(status: Int) {
         delegate?.sensorStatus(Int32(status))
     }
-    
+
+    // periphery:ignore:parameters data
     /// EEG取得開始、停止時のハンドリング
     private func handleEEGStatus(data: Data) {
 //        print(data.encodedUInt8)
     }
 }
 
-fileprivate extension Data {
+private extension Data {
     /// Data to [UInt8]
     var encodedUInt8: [UInt8] {
-        return self.withUnsafeBytes {
+        withUnsafeBytes {
             Array($0.bindMemory(to: UInt8.self)).map(UInt8.init(bigEndian:))
         }
     }
-    
+
     /// Data to [Int16]
     var encodedInt16: [Int16] {
-        return self.withUnsafeBytes {
+        withUnsafeBytes {
             Array($0.bindMemory(to: Int16.self)).map(Int16.init(bigEndian:))
         }
     }
